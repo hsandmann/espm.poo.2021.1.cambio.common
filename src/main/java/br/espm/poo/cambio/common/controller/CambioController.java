2704b05e0d1ca8feeda96e2a1aa7c189c547a6b0
Humberto Sandmann <hsandmann@ieee.org>
2021-06-07 22:48:44 -0300

    Endpoint simbolo for Currency

--- src/main/java/br/espm/poo/cambio/common/controller/CambioController.java
+++ src/main/java/br/espm/poo/cambio/common/controller/CambioController.java
@@ -4,6 +4,7 @@ import br.espm.poo.cambio.common.datatype.Cotacao;
 import br.espm.poo.cambio.common.datatype.Moeda;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -13,10 +14,17 @@ public interface CambioController {
     @GetMapping("/moedas")
     List<Moeda> moedas();
 
+    @GetMapping("/moedas/{simbolo}")
+    List<Moeda> moedas(String simbolo);
+
     @GetMapping("/cotacoes/{id}")
     Cotacao cotacao(String id);
 
-    @GetMapping("/cotacoes/{moeda}")
-    List<Cotacao> cotacoes(String moeda);
+    @GetMapping("/cotacoes")
+    List<Cotacao> cotacoes(
+            @RequestParam String simbolo,
+            @RequestParam String ini,
+            @RequestParam String fim
+    );
 
 }
